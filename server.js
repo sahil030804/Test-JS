8a37996eecaa07aef1873ad5e5a7679f34e5d7a7
Sahil Ranpariya <sahilranpariya60@gmail.com>
2024-09-30 12:27:22 +0530

    Add new staging data

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 console.log("it is server");
 console.log("Staging area");
 console.log("Auth created");
-console.log("new testing coe");
+console.log("new testing code");
+console.log("new testing code 1");
